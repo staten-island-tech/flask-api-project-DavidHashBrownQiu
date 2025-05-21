265b73163a119ed34a545bbb8ae18c4fee21dd2a
DavidHashBrownQiu <longhutch@gmail.com>
2025-05-21 10:15:50 -0400

    okclickingitclickinhnew

--- app.py
+++ app.py
@@ -6,10 +6,27 @@ app = Flask(__name__)
 # Route for the home page
 @app.route("/")
 def index():
-    response = requests.get("https://raw.githubusercontent.com/Hipo/university-domains-list/refs/heads/master/world_universities_and_domains.json")
+    response = requests.get("https://raw.githubusercontent.com/andyklimczak/TheReportOfTheWeek-API/refs/heads/master/data/reports.json")
     data = response.json()
+    reviews = data['reports']
 
-    universities = []
+    foods = [] #used to use the info needed
+
+    for review in reviews:
+        product = review['product']
+        manufacturer = review['manufacturer']
+        videoCode = review['videoCode']
+        imageUrl = f"https://img.youtube.com/vi/{videoCode}/0.jpg"  # note to self: this for loop helps w/ searching through revi4ews
+        #to put info into the list for each product of the databawswe
+        foods.append({
+            'name': product.capitalize(),
+            'company': manufacturer,
+            'image': imageUrl
+        })
+
+    return render_template("index.html", foods=foods)
+
+@app.route("/review/")
 
 if __name__ == '__main__':
     app.run(debug=True)
